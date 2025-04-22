b6f6181227d1f36fad51b773ac0a6fac3d3e23e8
vokol <vokol@softserveinc.com>
2025-04-22 15:01:25 +0300

    Cleanup

--- src/main/java/eu/flare/controller/ProjectController.java
+++ src/main/java/eu/flare/controller/ProjectController.java
@@ -11,7 +11,6 @@ import eu.flare.model.dto.EmptyProjectDto;
 import eu.flare.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
